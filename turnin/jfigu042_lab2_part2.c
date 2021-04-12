d88c569324f9a304962900c5172dc3690e2f59b3
Justin Figueroa <jfigu042@wch136-24.cs.ucr.edu>
2021-04-11 23:28:48 -0700

    Completed part 2

---
+++ turnin/jfigu042_lab2_part2.c
@@ -0,0 +1,35 @@
+/*	Author: jfigu042
+ *  Partner(s) Name: <none> 
+ *	Lab Section: 021
+ *	Assignment: Lab #2 Exercise #2
+ *	Exercise Description: Light that turns on if garage door is open at night
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0x00;
+    DDRC = 0x07; PORTC = 0x00;
+    /* Insert your solution below */
+    unsigned char tmpA = 0x00;
+    unsigned char cntavail = 0x00;
+    while (1) {
+        cntavail = 0x00;
+        tmpA = PINA;
+        int counter = 0;
+        for (counter = 0; counter < 4; counter++) {
+            if ((tmpA & 0x01) == 0x00) {
+                cntavail += 1;
+            }
+            tmpA = tmpA >> 1;
+        }
+        PORTC = cntavail;
+    }
+    return 1;
+}
