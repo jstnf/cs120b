df153e554623ec827a68bd4aa970f4d1ca46f807
Justin <jstnf@users.noreply.github.com>
2021-05-06 22:13:57 -0700

    Start lab6 pt2

--- Lab6_SynchSMs/source/main.c
+++ Lab6_SynchSMs/source/main.c
@@ -1,7 +1,7 @@
 /*	Author: jfigu042
  *  Partner(s) Name: 
  *	Lab Section: 021
- *	Assignment: Lab #6 Exercise #1
+ *	Assignment: Lab #6 Exercise #2
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -13,19 +13,36 @@
 #include "simAVRHeader.h"
 #endif
 
-enum SM_STATES { SM_SMStart, SM_Pattern1, SM_Pattern2, SM_Pattern3 } SM_STATE;
+enum SM_STATES { SM_SMStart, SM_Pattern1, SM_Pattern2Down, SM_Pattern2Up, SM_Pattern3, SM_Stopped, SM_Wait } SM_STATE;
 
 void TickFct_Pattern() {
     switch (SM_STATE) {
         case SM_SMStart:
-        case SM_Pattern3:
             SM_STATE = SM_Pattern1;
             break;
-        case SM_Pattern1:
-            SM_STATE = SM_Pattern2;
+        case SM_Pattern3:
+            SM_STATE = SM_Pattern2Up;
             break;
-        case SM_Pattern2:
+        case SM_Pattern1:
+            SM_STATE = SM_Pattern2Down;
+            break;
+        case SM_Pattern2Up:
+            SM_STATE = SM_Pattern1;
+            break;
+        case SM_Pattern2Down:
             SM_STATE = SM_Pattern3;
+            break;
+        case SM_Stopped:
+            if (~PINA & 0x01) {
+                SM_STATE = SM_Stopped; // Do nothing since button is still pressed
+            } else {
+                SM_STATE = SM_Wait; // Now transition to wait since button is not pressed
+            }
+            break;
+        case SM_Wait:
+            if (~PINA & 0x01) {
+                SM_STATE = SM_Pattern1;
+            }
             break;
     }
     
@@ -33,11 +50,14 @@ void TickFct_Pattern() {
         case SM_Pattern1:
             PORTB = 0x01;
             break;
-        case SM_Pattern2:
+        case SM_Pattern2Up:
+        case SM_Pattern2Down:
             PORTB = 0x02;
             break;
         case SM_Pattern3:
             PORTB = 0x04;
+            break;
+        default:
             break;
     }
 }
