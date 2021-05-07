f33aa8838d857dfa6f5e20861a44c085f8ba64f2
Justin <jstnf@users.noreply.github.com>
2021-05-06 23:50:00 -0700

    Start lab 7 part 3

--- Lab7_ADC/turnin/jfigu042_lab7_part2.c
+++ Lab7_ADC/turnin/jfigu042_lab7_part2.c
@@ -1,7 +1,7 @@
 /*	Author: jfigu042
  *  Partner(s) Name: 
  *	Lab Section: 021
- *	Assignment: Lab #7 Exercise #3
+ *	Assignment: Lab #7 Exercise #2
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -18,24 +18,21 @@ void ADC_init() {
     ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
 }
 
-unsigned short x = 0x0000;
-unsigned short MAX = 0x0300;
+unsigned short x = 0x00;
 
 int main(void) {
     /* Insert DDR and PORT initializations */
     DDRA = 0x00; PORTA = 0x00;
-    DDRB = 0x01; PORTB = 0x00;
+    DDRB = 0xFF; PORTB = 0x00;
+    DDRD = 0x03; PORTD = 0x00;
 
     ADC_init();
 
     /* Insert your solution below */
     while (1) {
         x = ADC;
-        if (x >= (MAX/2)) {
-            PORTB = 0x01;
-        } else {
-            PORTB = 0x00;
-        }
+        PORTB = (char) x;
+        PORTD = (char) (x >> 8);
     }
     return 1;
 }
