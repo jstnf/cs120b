3ce6245d5248600cce8a1b98bda85910a0dced19
Justin <jstnf@users.noreply.github.com>
2021-05-23 21:06:22 -0700

    Use logical GetBit instead of function

--- Lab10_Scheduler/source/main.c
+++ Lab10_Scheduler/source/main.c
@@ -15,31 +15,31 @@
 unsigned char GetKeypadKey() {
     PORTC = 0xEF;
     asm("nop");
-    if (GetBit(PINC,0)==0) return('1');
-    if (GetBit(PINC,1)==0) return('4');
-    if (GetBit(PINC,2)==0) return('7');
-    if (GetBit(PINC,3)==0) return('*');
+    if ((PINC & 0x01) == 0x01) return('1');
+    if ((PINC & 0x02) == 0x02) return('4');
+    if ((PINC & 0x04) == 0x04) return('7');
+    if ((PINC & 0x08) == 0x08) return('*');
     
     PORTC = 0xDF;
     asm("nop");
-    if (GetBit(PINC,0)==0) return('2');
-    if (GetBit(PINC,1)==0) return('5');
-    if (GetBit(PINC,2)==0) return('8');
-    if (GetBit(PINC,3)==0) return('0');
+    if ((PINC & 0x01) == 0x01) return('2');
+    if ((PINC & 0x02) == 0x02) return('5');
+    if ((PINC & 0x04) == 0x04) return('8');
+    if ((PINC & 0x08) == 0x08) return('0');
     
     PORTC = 0xBF;
     asm("nop");
-    if (GetBit(PINC,0)==0) return('3');
-    if (GetBit(PINC,1)==0) return('6');
-    if (GetBit(PINC,2)==0) return('9');
-    if (GetBit(PINC,3)==0) return('#');
+    if ((PINC & 0x01) == 0x01) return('3');
+    if ((PINC & 0x02) == 0x02) return('6');
+    if ((PINC & 0x04) == 0x04) return('9');
+    if ((PINC & 0x08) == 0x08) return('#');
     
     PORTC = 0x7F;
     asm("nop");
-    if (GetBit(PINC,0)==0) return('A');
-    if (GetBit(PINC,1)==0) return('B');
-    if (GetBit(PINC,2)==0) return('C');
-    if (GetBit(PINC,3)==0) return('D');
+    if ((PINC & 0x01) == 0x01) return('A');
+    if ((PINC & 0x02) == 0x02) return('B');
+    if ((PINC & 0x04) == 0x04) return('C');
+    if ((PINC & 0x08) == 0x08) return('D');
     
     return('\0');
 }
