da04ba9c7c62c792cb920c121f6cd64922a37af5
Justin <jstnf@users.noreply.github.com>
2021-06-06 18:38:02 -0700

    Lab 11 LED demo

--- Lab11_Pong/source/main.c
+++ Lab11_Pong/source/main.c
@@ -1,23 +1,120 @@
-/*	Author: lab
+/*	Author: jfigu042
  *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
- *	Exercise Description: [optional - include for your own benefit]
+ *	Lab Section: 021
+ *	Assignment: Lab #11 Exercise #1
+ *	Exercise Description: Class Pong Game Implementation
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
+ *
+ *  Demo Link: <>
  */
 #include <avr/io.h>
+#include "pwm.h"
+#include "timer.h"
+#include "scheduler.h"
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
+//--------------------------------------
+// LED Matrix Demo SynchSM
+// Period: 100 ms
+//--------------------------------------
+enum Demo_States {shift};
+int Demo_Tick(int state) {
+
+	// Local Variables
+	static unsigned char pattern = 0x80;	// LED pattern - 0: LED off; 1: LED on
+	static unsigned char row = 0xFE;  	// Row(s) displaying pattern. 
+							// 0: display pattern on row
+							// 1: do NOT display pattern on row
+	// Transitions
+	switch (state) {
+		case shift:	
+        break;
+		default:	
+            state = shift;
+			break;
+	}	
+	// Actions
+	switch (state) {
+        case shift:	
+            if (row == 0xEF && pattern == 0x01) { // Reset demo 
+				pattern = 0x80;		    
+				row = 0xFE;
+			} else if (pattern == 0x01) { // Move LED to start of next row
+				pattern = 0x80;
+				row = (row << 1) | 0x01;
+			} else { // Shift LED one spot to the right on current row
+				pattern >>= 1;
+			}
+			break;
+		default:
+	break;
+	}
+	PORTC = pattern;	// Pattern to display
+	PORTD = row;		// Row(s) displaying pattern	
+	return state;
+}
+
+
+// SM to create outputs (req. since lab 9)
+enum SM0_STATES { SM0_SMStart, SM0_Combine };
+int TickFct_Combine(int state) {
+    switch (state) {
+        default:
+            state = SM0_Combine;
+            break;
+    }
+    
+    switch (state) {
+        case SM0_Combine:
+            break;
+        default:
+            break;
+    }
+    
+    return state;
+}
+
 int main(void) {
     /* Insert DDR and PORT initializations */
+    DDRC = 0xFF; PORTC = 0x00;
+    DDRD = 0xFF; PORTD = 0x00;
 
     /* Insert your solution below */
+    static task task0, demo;
+    task *tasks[] = { &task0, &demo };
+    const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
+    
+    const char start = 0;
+    task0.state = start;
+    task0.period = 1;
+    task0.elapsedTime = task0.period;
+    task0.TickFct = &TickFct_Combine;
+    
+    demo.state = start;
+    demo.period = 100;
+    demo.elapsedTime = demo.period;
+    demo.TickFct = &Demo_Tick;
+    
+    TimerSet(1);
+    TimerOn();
+    PWM_on();
+    
+    unsigned short i;
     while (1) {
-
+        for (i = 0; i < numTasks; i++) {
+            if (tasks[i]->elapsedTime == tasks[i]->period) {
+                tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
+                tasks[i]->elapsedTime = 0;
+            }
+            tasks[i]->elapsedTime += 1;
+        }
+        
+        while (!TimerFlag);
+        TimerFlag = 0;
     }
     return 1;
 }
