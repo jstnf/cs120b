b235fec6ee18fee75d05d81f1a94bb5f0108dfe4
Justin <jstnf@users.noreply.github.com>
2021-06-06 20:12:17 -0700

    Game baseplate and basic drawing logic

--- Lab11_Pong/source/main.c
+++ Lab11_Pong/source/main.c
@@ -17,16 +17,181 @@
 #include "simAVRHeader.h"
 #endif
 
-unsigned char row1 = 0x11;
-unsigned char row2 = 0x22;
-unsigned char row3 = 0x43;
-unsigned char row4 = 0x84;
-unsigned char row5 = 0x15;
+// "2D Matrix" contains patterns for each row
+unsigned char row1 = 0x00;
+unsigned char row2 = 0x00;
+unsigned char row3 = 0x00;
+unsigned char row4 = 0x00;
+unsigned char row5 = 0x00;
 
+// Pong implementation
+unsigned char scoreP1 = 0x00;
+unsigned char scoreP2 = 0x00;
+unsigned int pongGameTick = 0;
+
+unsigned char ball_vel = 0x11; // [4] is X vel - 0 moves left, 1 moves right; [1:0] is Y vel - 0 ball moves up, 1 ball moves straight, 2 ball moves down
+unsigned char ball_pos = 0x33; // [7:4] is X pos; [3:0] is Y pos - 0 is top border, game area 1-5, 6 is bottom border
+
+unsigned char paddles_pos = 0x33; // [7:4] is P1 pos; [3:0] is P2 pos - 2 is top position, 3 is middle position, 4 is bottom position
+
+// GAME LOGIC
+void fullResetGame() {
+    scoreP1 = 0x00;
+    scoreP2 = 0x00;
+    pongGameTick = 0;
+}
+
+void roundResetGame() {
+    ball_vel = 0x11;
+    ball_pos = 0x33;
+    paddles_pos = 0x33;
+}
+
+void PongTick(int tickNum) {
+    // The first tick will reset the game to starting state, but maintain score
+    if (tick == 1) {
+        roundResetGame();
+    }
+}
+
+// LED LOGIC
+void clearCanvas() {
+    row1 = 0x00;
+    row2 = 0x00;
+    row3 = 0x00;
+    row4 = 0x00;
+    row5 = 0x00;
+}
+
+// Clear the canvas before you call this method!
+void drawPaddles() {
+    // In all situations we draw the middle dot on both sides
+    row3 = row3 | 0x81; // O******O
+    
+    // Left paddle
+    switch (paddles_pos & 0xF0) {
+        case 0x20:
+            row1 = row1 | 0x80;
+            row2 = row2 | 0x80;
+            break;
+        case 0x30:
+            row2 = row2 | 0x80;
+            row4 = row4 | 0x80;
+            break;
+        case 0x40:
+            row4 = row4 | 0x80;
+            row5 = row5 | 0x80;
+            break;
+    }
+    
+    // Right paddle
+    switch (paddles_pos & 0x0F) {
+        case 0x02:
+            row1 = row1 | 0x01;
+            row2 = row2 | 0x01;
+            break;
+        case 0x03:
+            row2 = row2 | 0x01;
+            row4 = row4 | 0x01;
+            break;
+        case 0x04:
+            row4 = row4 | 0x01;
+            row5 = row5 | 0x01;
+            break;
+    }
+}
+
+// Clear the canvas before you call this method!
+void drawBall() {
+    unsigned char ballPattern = 0x80;
+    unsigned char xPos = ball_pos >> 4;
+    unsigned char i;
+    for (i = 0; i < xPos; i++) {
+        ballPattern = ballPattern >> 1;
+    }
+    
+    switch (ball_pos & 0x0F) { // Determine which row to draw to
+        case 0x01:
+            row1 = row1 | ballPattern;
+            break;
+        case 0x02:
+            row2 = row2 | ballPattern;
+            break;
+        case 0x03:
+            row3 = row3 | ballPattern;
+            break;
+        case 0x04:
+            row4 = row4 | ballPattern;
+            break;
+        case 0x05:
+            row5 = row5 | ballPattern;
+            break;
+        default:
+            break;
+    }
+}
+
+void PongDraw() {
+    clearCanvas();
+    drawBall();
+    drawPaddles();
+}
+
+void drawStartScreen() {
+    row1 = 0x00; // ********
+    row2 = 0x24; // **O**O**
+    row3 = 0x00; // ********
+    row4 = 0x42; // *O****O*
+    row5 = 0x3C; // **OOOO**
+}
+
+// SM to control game states
+enum SM2_STATES { SM2_MenuRise, SM2_MenuFall, SM2_PlayingRise, SM2_PlayingFall }
+int TickFct_GameState(int state) {
+    switch (state) {
+        default:
+            if ((~PINA & 0x80) == 0x00) state = SM2_MenuFall;
+            else state = SM2_MenuRise;
+            break;
+        case SM2_MenuFall:
+            if (~PINA & 0x80) {
+                state = SM2_PlayingRise;
+                
+                // Only want to reset these values on transition
+                fullResetGame();
+            }
+            break;
+        case SM2_PlayingRise:
+            if ((~PINA & 0x80) == 0x00) state = SM2_PlayingFall;
+            break;
+        case SM2_PlayingFall:
+            if (~PINA & 0x80) state = SM2_MenuRise;
+            break;
+    }
+    
+    switch (state) {
+        case SM2_PlayingRise:
+            PongDraw();
+            break;
+        case SM2_PlayingFall:
+            pongGameTick++;
+            PongTick(pongGameTick);
+            PongDraw();
+            break;
+        default:
+            drawStartScreen();
+            break;
+    }
+    
+    return state;
+}
+
+// Global variables controlling to which row and what pattern to draw
 unsigned char row = 0x01;
 unsigned char pattern = 0x00;
 
-enum SM1_LED { SM1_SMStart, SM1_R1, SM1_R2, SM1_R3, SM1_R4, SM1_R5 };
+// SM to alternate row and pattern drawn to board, sets up variables for task0 to output
+enum SM1_STATES { SM1_SMStart, SM1_R1, SM1_R2, SM1_R3, SM1_R4, SM1_R5 };
 int TickFct_DrawLED(int state) {
     switch (state) {
         default:
@@ -95,12 +260,13 @@ int TickFct_Combine(int state) {
 
 int main(void) {
     /* Insert DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0x11;
     DDRC = 0xFF; PORTC = 0x00;
     DDRD = 0xFF; PORTD = 0x00;
 
     /* Insert your solution below */
-    static task task0, task1;
-    task *tasks[] = { &task0, &task1 };
+    static task task0, task1, task2;
+    task *tasks[] = { &task2, &task1, &task0 }; // Task execution order
     const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
     
     const char start = 0;
@@ -113,6 +279,11 @@ int main(void) {
     task1.period = 1;
     task1.elapsedTime = task1.period;
     task1.TickFct = &TickFct_DrawLED;
+    
+    task2.state = start;
+    task2.period = 1;
+    task2.elapsedTime = task2.period;
+    task2.TickFct = &TickFct_GameState;
     
     TimerSet(1);
     TimerOn();
